6f0d2db66645dbeb69bf6e72a2141b945d956546
LaxmiPranaviBairi <l9vi139@gmail.com>
2026-08-23 11:49:34 +0530

    query params and product routes

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,25 @@ router.get('/bye/:name',(req,res)=>{
     res.send(`Bye,${name}!`);
 });
 
+router.get('/search',(req,res)=>{
+    const {term,limit} = req.query;
+    res.json({searchedFor: term, limit: limit});
+});
+
+const products=[
+    {id: 1,name: 'Laptop',price: 50000},
+    {id: 2,name: 'Phone',price: 20000},
+    {id: 3,name:'Headphones',price: 2000},
+];
+
+router.get('/products/:id',(req,res)=>{
+   const id=Number(req.params.id);
+   const product=products.find(p=>p.id===id);
+
+   if(!product){
+    return res.status(404).json({message:'Product not found'});
+   }
+   res.json(product);
+});
+
 module.exports = router
